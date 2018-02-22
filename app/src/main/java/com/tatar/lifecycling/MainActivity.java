4b3414e5b331863db7b4e818705b1415906bf278
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-22 12:06:36 +0300

    filled MainActivity

--- app/src/main/java/com/tatar/lifecycling/MainActivity.java
+++ app/src/main/java/com/tatar/lifecycling/MainActivity.java
@@ -1,13 +1,38 @@
 package com.tatar.lifecycling;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LifecyclingActivity {
+
+    private Button activityAButton;
+    private Button activityBButton;
+
+    public MainActivity() {
+        setChildActivityName("MainActivity");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityAButton = findViewById(R.id.a_button);
+        activityBButton = findViewById(R.id.b_button);
+
+        activityAButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        activityBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
