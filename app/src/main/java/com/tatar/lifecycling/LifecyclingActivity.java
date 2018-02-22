04c287bd0eadf31697cad1c7c67f3bbc97e9e64d
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-22 17:06:32 +0300

    added lifecycle method explanations

--- app/src/main/java/com/tatar/lifecycling/LifecyclingActivity.java
+++ app/src/main/java/com/tatar/lifecycling/LifecyclingActivity.java
@@ -1,7 +1,6 @@
 package com.tatar.lifecycling;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -15,42 +14,85 @@ public class LifecyclingActivity extends AppCompatActivity {
         this.childActivityName = childActivityName;
     }
 
+    /*
+    called when the activity is first created
+    always followed by onStart
+    create views, bind data to lists, create threads etc. here
+    provides a bundle which contains the frozen state of the activity(if there is one)
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, childActivityName + " onCreate called");
         super.onCreate(savedInstanceState);
     }
 
+    /*
+    called when the activity is becoming visible to user
+    followed by on resume if the activity comes to foreground or onStop if it becomes hidden
+    As an example, a BroadcastReceiver can be registered here
+    */
     @Override
     protected void onStart() {
         Log.i(TAG, childActivityName + " onStart called");
         super.onStart();
     }
 
+    /*
+    called when the activity becomes visible and interacts with the user
+    at this point the activity is on top of the activity stack
+    always followed by onStop
+     */
     @Override
     protected void onResume() {
         Log.i(TAG, childActivityName + " onResume called");
         super.onResume();
     }
 
+    /*
+    called when the system is about to start resuming a previous activity(when the current activity is removed from the top of activity stack)
+    called when user can no longer interact with the activity
+    followed by either onResume or onStop
+    used to commit unsaved changes to persistent data, stop animations, or other things that may be consuming CPU here
+     implementation is that callback must be lightweight because the next activity wont be resumed until this method returns
+     */
     @Override
     protected void onPause() {
         Log.i(TAG, childActivityName + " onPause called");
         super.onPause();
     }
 
+    /*
+    called when the activity is no longer visible to the user which means another activity has been resumed  and covering this one
+    (New activity is being started, an existing one is being brought to foreground, or activity is being destroyed)
+    Followed by either onRestart or if the activity is coming back or onDestroy if the activity is going away
+    As an example, a BroadcastReceiver can be unregistered here
+    KILLABLE (Pre-HONEYCOMB)
+     */
     @Override
     protected void onStop() {
         Log.i(TAG, childActivityName + " onStop called");
         super.onStop();
     }
 
+    /*
+    final call before the activity is destroyed
+    can be called because someone called finish() method or activity is destroying the activity due to lack of resources
+    The scenario can be distinguished by isFinishing() method
+    hitting back button also calls onDestroy
+    As an example, a running thread can be stopped here
+    KILLABLE
+     */
     @Override
     protected void onDestroy() {
         Log.i(TAG, childActivityName + " onDestroy called");
         super.onDestroy();
     }
 
+    /*
+    called after the activity has been stop, prior to it being started again
+    always followed by onStart
+    KILLABLE
+     */
     @Override
     protected void onRestart() {
         Log.i(TAG, childActivityName + " onRestart called");
@@ -58,12 +100,25 @@ public class LifecyclingActivity extends AppCompatActivity {
     }
 
     // These two are not Activity lifecycle methods
+
+    /*
+    called to retrieve per-instance state from an activity before being killed so that the state can be restored in onCreate(Bundle) or onRestoreInstanceState(Bundle)
+    (the Bundle populated by this method will be passed to both)
+    The default implementation takes care of most of the UI per-instance state for you by calling onSaveInstanceState() on each view in the hierarchy that has an id
+    if you override this method to save additional information not captured by each individual view, you will likely want to call through to the default implementation, otherwise be prepared to save all of the state of each view yourself.
+    if called, this method will occur before onStop(). There are no guarantees about whether it will occur before or after onPause()
+     */
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    protected void onSaveInstanceState(Bundle outState) {
         Log.i(TAG, childActivityName + " onSaveInstanceState called");
-        super.onSaveInstanceState(outState, outPersistentState);
+        super.onSaveInstanceState(outState);
     }
 
+    /*
+    this method is called after onStart() when the activity is being re-initialized from a previously saved state, given here in savedInstanceState.
+    the default implementation of this method performs a restore of any view state that had previously been frozen by onSaveInstanceState(Bundle).
+    this method is called between onStart() and onPostCreate(Bundle).
+    */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         Log.i(TAG, childActivityName + " onRestoreInstanceState called");
