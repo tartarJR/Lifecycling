60a08b0fd718dc4c91825aac102b3f927b0d05fb
tartarJr <mustafaogun.ozturk@gmail.com>
2018-02-23 14:31:20 +0300

    shared preferences and data persistence sample is added

--- app/src/main/java/com/tatar/lifecycling/MainActivity.java
+++ app/src/main/java/com/tatar/lifecycling/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tatar.lifecycling;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,10 @@ public class MainActivity extends LifecyclingActivity {
 
     private static final String CLASS_NAME = MainActivity.class.getSimpleName();
     private static final String DUMMY_CONTENT = "DUMMY_CONTENT";
+    private static final String PREF_FILE = "com.tatar.lifecycling.prefs";
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     private EditText dummyEditText;
     private Button dopeActivityButton;
@@ -23,9 +29,15 @@ public class MainActivity extends LifecyclingActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         dummyEditText = findViewById(R.id.dummyEditText);
         dopeActivityButton = findViewById(R.id.dope_activity_button);
         transparentActivityButton = findViewById(R.id.transparent_activity_button);
+
+        String dummyText = sharedPreferences.getString(DUMMY_CONTENT, "");
+        dummyEditText.setText(dummyText);
 
         dopeActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +52,13 @@ public class MainActivity extends LifecyclingActivity {
                 startActivity(MainActivity.this, TransparentActivity.class);
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        editor.putString(DUMMY_CONTENT, dummyEditText.getText().toString());
+        editor.apply();
+
+        super.onPause();
     }
 }
